676492ca373b27c745278f27ab099b6849f2be95
Mikkel Isaksen Jensen <mikkelisaksen88@gmail.com>
2019-10-29 11:01:31 +0100

    project startup

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Assignment2_EFcore_au529152
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is Assignment 2!");
+
+            using (var context = new AppDbContext)
+        }
+    }
+}
